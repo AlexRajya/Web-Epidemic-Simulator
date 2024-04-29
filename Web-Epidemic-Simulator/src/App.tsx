41351f093d0225b43dd0094d4aaa7c934be839f7
Alex Rajya <rajyaalex@gmail.com>
2024-04-29 23:48:39 +0100

    Add basic visualisation and fix reset button

--- Web-Epidemic-Simulator/src/App.tsx
+++ Web-Epidemic-Simulator/src/App.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import { Grid } from './Simulation/Grid';
 import { Configuration } from './Simulation/Configuration';
+import { ConvertTo2DArray } from './Simulation/Helpers/ConvertTo2DArray';
+import { Cell } from './Simulation/Cell';
 
 function App() {
   const [grid, setGrid] = useState<Grid>(new Grid());
@@ -18,40 +20,77 @@ function App() {
     setGrid(startGrid);
   }, []);
 
+  const cellColour = (cell: Cell): string => {
+      let colour = "gray";
+      if(cell.PopulationCount){
+        colour = "white";
+      }
+
+      const percentInfected = ((cell.IncubatedCount + cell.InfectedCount) / cell.PopulationCount) * 100;
+      if(percentInfected > 90){
+        colour = "red";
+      }else if(percentInfected > 60){
+        colour = "orange";
+      }else if(percentInfected > 30){
+        colour = "yellow";
+      }
+
+      return colour
+  }
+
   return (
-    <>
-      <button onClick={() => {
-          grid.Next(config);
-          setPopulationCount(grid.PopulationOverallCount);
-          setSusceptibleCount(grid.SusceptibleOverallCount);
-          setInfectedCount(grid.InfectedOverallCount);
-          setIncubatedCount(grid.IncubatedOverallCount);
-          setRecoveredCount(grid.RecoveredOverallCount);
-        }
-      }>
-        Next day
-      </button>
-      <p>
-        Total population: {populationCount}
-      </p>
-      <p>
-        Total susceptible: {susceptibleCount}
-      </p>
-      <p>
-        Total incubating: {incubatedCount}
-      </p>
-      <p>
-        Total infected: {infectedCount}
-      </p>
-      <p>
-        Total recovered: {recoveredCount}
-      </p>
-      <button onClick={() => {
-        setGrid(new Grid())}
+    <div style={{display: "flex"}}>
+      <div style={{width: "600px"}}>
+        {ConvertTo2DArray(grid.cells).map((rowOfCells) => 
+          <div style={{display: 'flex'}}>
+            {rowOfCells.map((cell) => 
+              <div style={{ backgroundColor: cellColour(cell), border: "1px solid black", width: "10px", height: "10px"}} /> 
+            )}
+          </div>
+        )}
+      </div>
+      <div style={{width: "300px"}}>
+        <button onClick={() => {
+            grid.Next(config);
+            setPopulationCount(grid.PopulationOverallCount);
+            setSusceptibleCount(grid.SusceptibleOverallCount);
+            setInfectedCount(grid.InfectedOverallCount);
+            setIncubatedCount(grid.IncubatedOverallCount);
+            setRecoveredCount(grid.RecoveredOverallCount);
+          }
         }>
-        Reset
-      </button>
-    </>
+          Next day
+        </button>
+        <p>
+          Total population: {populationCount}
+        </p>
+        <p>
+          Total susceptible: {susceptibleCount}
+        </p>
+        <p>
+          Total incubating: {incubatedCount}
+        </p>
+        <p>
+          Total infected: {infectedCount}
+        </p>
+        <p>
+          Total recovered: {recoveredCount}
+        </p>
+        <button onClick={() => {
+            const startGrid = new Grid();
+            setPopulationCount(startGrid.PopulationOverallCount);
+            setSusceptibleCount(startGrid.SusceptibleOverallCount);
+            setInfectedCount(startGrid.InfectedOverallCount);
+            setIncubatedCount(startGrid.IncubatedOverallCount);
+            setRecoveredCount(startGrid.RecoveredOverallCount);
+            setGrid(startGrid);
+            startGrid.SetAsInfected(1001);
+          } 
+        }>
+          Reset
+        </button>
+      </div>
+    </div>
   )
 }
 
