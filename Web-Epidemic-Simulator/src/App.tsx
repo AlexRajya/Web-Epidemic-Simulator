26549df42d85b6ca6fbb02e9e0dae18352283756
Alex Rajya <rajyaalex@gmail.com>
2024-05-02 01:12:48 +0100

    Add MUI and basic app bar

--- Web-Epidemic-Simulator/src/App.tsx
+++ Web-Epidemic-Simulator/src/App.tsx
@@ -5,6 +5,8 @@ import { Configuration } from './Simulation/Configuration';
 import { ConvertTo2DArray } from './Simulation/Helpers/ConvertTo2DArray';
 import { Cell } from './Simulation/Cell';
 import { cellsPopulation } from './Simulation/Data/GermanyPopulationDensity';
+import { AppBar, Box, Button, IconButton, Toolbar, Typography } from '@mui/material';
+import CoronavirusIcon from '@mui/icons-material/Coronavirus';
 
 function App() {
   const cols = 36, rows = 36;
@@ -49,53 +51,68 @@ function App() {
   }
 
   return (
-    <div style={{display: "flex"}}>
-      <div style={{width: "600px"}}>
-        {ConvertTo2DArray(grid.cells, rows).map((rowOfCells, index) => 
-          <div style={{display: 'flex'}} key={"row-" + index}>
-            {rowOfCells.map((cell) => 
-              <div 
-                style={{ backgroundColor: cellColour(cell), border: "1px solid black", width: "10px", height: "10px"}} 
-                key={"cell-" + cell.Index}
-              /> 
-            )}
-          </div>
-        )}
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <CoronavirusIcon fontSize='large'/>
+            <Typography variant="h4" component="div" sx={{ flexGrow: 1, textAlign:"left", padding: "10px" }}>
+              Epidemic Simulation
+            </Typography>
+            <Button variant="text" href='https://github.com/AlexRajya/Web-Epidemic-Simulator' sx={{color: "white"}}>
+              Source
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <div style={{display: "flex", alignContent: "center", justifyContent: "center", marginTop: "10%"}}>
+        <div>
+          {ConvertTo2DArray(grid.cells, rows).map((rowOfCells, index) => 
+            <div style={{display: 'flex'}} key={"row-" + index}>
+              {rowOfCells.map((cell) => 
+                <div 
+                  style={{ backgroundColor: cellColour(cell), border: "1px solid black", width: "10px", height: "10px"}} 
+                  key={"cell-" + cell.Index}
+                /> 
+              )}
+            </div>
+          )}
+        </div>
+        <div style={{width: "300px", marginTop: "auto", marginBottom: "auto"}}>
+          <Button variant="contained" onClick={() => {
+              grid.Next(config);
+              updateCounts(grid);
+            }
+          }>
+            Next day
+          </Button>
+          <p>
+            Total population: {populationCount}
+          </p>
+          <p>
+            Total susceptible: {susceptibleCount}
+          </p>
+          <p>
+            Total incubating: {incubatedCount}
+          </p>
+          <p>
+            Total infected: {infectedCount}
+          </p>
+          <p>
+            Total recovered: {recoveredCount}
+          </p>
+          <Button variant="contained" onClick={() => {
+              const startGrid = new Grid(rows, cols, cellsPopulation);
+              updateCounts(startGrid);
+              setGrid(startGrid);
+              startGrid.SetAsInfected(1001);
+            } 
+          }>
+            Reset
+          </Button>
+        </div>
       </div>
-      <div style={{width: "300px"}}>
-        <button onClick={() => {
-            grid.Next(config);
-            updateCounts(grid);
-          }
-        }>
-          Next day
-        </button>
-        <p>
-          Total population: {populationCount}
-        </p>
-        <p>
-          Total susceptible: {susceptibleCount}
-        </p>
-        <p>
-          Total incubating: {incubatedCount}
-        </p>
-        <p>
-          Total infected: {infectedCount}
-        </p>
-        <p>
-          Total recovered: {recoveredCount}
-        </p>
-        <button onClick={() => {
-            const startGrid = new Grid(rows, cols, cellsPopulation);
-            updateCounts(startGrid);
-            setGrid(startGrid);
-            startGrid.SetAsInfected(1001);
-          } 
-        }>
-          Reset
-        </button>
-      </div>
-    </div>
+    </>
   )
 }
 
