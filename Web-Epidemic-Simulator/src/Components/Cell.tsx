db2f371a4df8bd39b35b89d3960d79ead46e4350
Alex Rajya <rajyaalex@gmail.com>
2024-05-11 20:19:17 +0100

    Improve infected density colouring

--- Web-Epidemic-Simulator/src/Components/Cell.tsx
+++ Web-Epidemic-Simulator/src/Components/Cell.tsx
@@ -1,28 +1,66 @@
 import React from 'react';
 import { Cell as CellClass } from '../Simulation/Cell.tsx';
+import Popover from '@mui/material/Popover';
+import { Typography } from '@mui/material';
+import './Cell.css';
 
 export interface CellProps {
     cell: CellClass
 }
 
 const Cell: React.FC<CellProps> = ({ cell }: CellProps) => {
+    const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
+
+    const handlePopoverOpen = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handlePopoverClose = () => {
+        setAnchorEl(null);
+    };
+
+    const open = Boolean(anchorEl);
+
     const cellColour = (cell: CellClass): string => {  
         const percentInfected = ((cell.IncubatedCount + cell.InfectedCount) / cell.PopulationCount) * 100;
-        if(percentInfected > 90){
-          return "red";
-        }else if(percentInfected > 60){
-          return "orange";
-        }else if(percentInfected > 30){
-          return "yellow";
-        }else {
-            return cell.PopulationCount ? "white" : "gray";
-        }
+        return `rgb(${100 + percentInfected}, ${100 - percentInfected}, ${100 - percentInfected})`;
     }
 
     return (
-        <div 
-            style={{ backgroundColor: cellColour(cell), border: "1px solid black", width: "10px", height: "10px"}} 
-        /> 
+        <>
+            <div
+                className='Cell'
+                onMouseEnter={handlePopoverOpen}
+                onMouseLeave={handlePopoverClose}
+                style={{ backgroundColor: cellColour(cell)}} 
+            /> 
+            {cell.PopulationCount > 0 && (<Popover
+                id="mouse-over-popover"
+                sx={{
+                    pointerEvents: 'none',
+                }}
+                open={open}
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                }}
+                onClose={handlePopoverClose}
+                disableRestoreFocus
+            >
+                <Typography sx={{backgroundColor: "#242424", color: "white", padding: "4px"}}>
+                    Population: {cell.PopulationCount}<br/>
+                    Susceptible: {cell.SusceptibleCount}<br/>
+                    Incubated: {cell.IncubatedCount}<br/>
+                    Infected: {cell.InfectedCount}<br/>
+                    Recovered: {cell.RecoveredCount}
+                </Typography>
+            </Popover>)}
+        </>
     );
 };
 
