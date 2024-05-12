8ff1e8ff8067f7e40b6d1df29ca5ae37e7261dd2
Alex Rajya <rajyaalex@gmail.com>
2024-05-12 02:55:17 +0100

    Add icons to header bar items

--- Web-Epidemic-Simulator/src/Components/HeaderBar.tsx
+++ Web-Epidemic-Simulator/src/Components/HeaderBar.tsx
@@ -1,5 +1,7 @@
 import { Box, AppBar, Toolbar, Typography, Button } from '@mui/material';
 import CoronavirusIcon from '@mui/icons-material/Coronavirus';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import InfoIcon from '@mui/icons-material/Info';
 import React from 'react';
 
 const HeaderBar: React.FC = () => {
@@ -11,11 +13,11 @@ const HeaderBar: React.FC = () => {
                     <Typography variant="h4" component="div" sx={{ flexGrow: 1, textAlign:"left", padding: "10px" }}>
                         Epidemic Simulation
                     </Typography>
-                    <Button color="inherit">
+                    <Button startIcon={<InfoIcon />} color="inherit">
                         About
                     </Button>
-                    <Button color="inherit" href='https://github.com/AlexRajya/Web-Epidemic-Simulator' sx={{color: "white"}}>
-                        Source
+                    <Button startIcon={<GitHubIcon/>} color="inherit" href='https://github.com/AlexRajya/Web-Epidemic-Simulator' sx={{color: "white"}}>
+                        Source 
                     </Button>
                 </Toolbar>
             </AppBar>
