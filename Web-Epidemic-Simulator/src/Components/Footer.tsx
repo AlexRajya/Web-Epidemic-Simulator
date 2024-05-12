e65ecfe5b0d005a95ac88922e28eed3a2b916387
Alex Rajya <rajyaalex@gmail.com>
2024-05-12 02:55:08 +0100

    Add copyright footer

---
+++ Web-Epidemic-Simulator/src/Components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer: React.FC = () => {
+    return (
+        <footer>
+            Ⓒ Copyright Alex Rajya 2024 | All Rights Reserved
+        </footer>
+    );
+};
+
+export default Footer;
