fb33bffc6127e774a81d6777b7af4232086a834d
Alex Rajya <rajyaalex@gmail.com>
2024-05-13 20:18:15 +0100

    Add version bump action and display version in footer

--- Web-Epidemic-Simulator/src/Components/Footer.tsx
+++ Web-Epidemic-Simulator/src/Components/Footer.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./Footer.css";
+import packageJson from "../../package.json";
 
 const Footer: React.FC = () => {
-  return <footer>Ⓒ Copyright Alex Rajya 2024 | All Rights Reserved</footer>;
+  return (
+    <footer>
+      Ⓒ Copyright Alex Rajya 2024 | All Rights Reserved | v{packageJson.version}
+    </footer>
+  );
 };
 
 export default Footer;
